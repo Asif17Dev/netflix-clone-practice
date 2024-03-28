c81398776770c69c0e3e1269bfe1078ed322c263
Ashif <abdeasif17@gmail.com>
2024-03-28 22:09:07 +0530

    error fix

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -54,7 +54,7 @@ function Modal() {
     <MuiModal
       open={showModal}
       onClose={handleClose}
-      className="fixex !top-7 left-0 right-0 z-50 mx-auto  w-full max-w-5xl overflow-hidden 
+      className="fixed !top-7 left-0 right-0 z-50 mx-auto  w-full max-w-5xl overflow-hidden 
       overflow-y-scroll rounded-sm scrollbar-hide"
     >
       <>
--- pages/index.tsx
+++ pages/index.tsx
@@ -68,36 +68,40 @@ const Home = ({
 export default Home
 
 export const getServerSideProps = async () => {
-  const [
-    netflixOriginals,
-    trendingNow,
-    topRated,
-    actionMovies,
-    comedyMovies,
-    horrorMovies,
-    romanceMovies,
-    documentaries,
-  ] = await Promise.all([
-    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
-    fetch(requests.fetchTrending).then((res) => res.json()),
-    fetch(requests.fetchTopRated).then((res) => res.json()),
-    fetch(requests.fetchActionMovies).then((res) => res.json()),
-    fetch(requests.fetchComedyMovies).then((res) => res.json()),
-    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
-    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
-    fetch(requests.fetchDocumentaries).then((res) => res.json()),
-  ])
+  try {
+    const [
+      netflixOriginals,
+      trendingNow,
+      topRated,
+      actionMovies,
+      comedyMovies,
+      horrorMovies,
+      romanceMovies,
+      documentaries,
+    ] = await Promise.all([
+      fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+      fetch(requests.fetchTrending).then((res) => res.json()),
+      fetch(requests.fetchTopRated).then((res) => res.json()),
+      fetch(requests.fetchActionMovies).then((res) => res.json()),
+      fetch(requests.fetchComedyMovies).then((res) => res.json()),
+      fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+      fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+      fetch(requests.fetchDocumentaries).then((res) => res.json()),
+    ])
 
-  return {
-    props: {
-      netflixOriginals: netflixOriginals.results,
-      trendingNow: trendingNow.results,
-      topRated: topRated.results,
-      actionMovies: actionMovies.results,
-      comedyMovies: comedyMovies.results,
-      horrorMovies: horrorMovies.results,
-      romanceMovies: romanceMovies.results,
-      documentaries: documentaries.results,
-    },
+    return {
+      props: {
+        netflixOriginals: netflixOriginals.results,
+        trendingNow: trendingNow.results,
+        topRated: topRated.results,
+        actionMovies: actionMovies.results,
+        comedyMovies: comedyMovies.results,
+        horrorMovies: horrorMovies.results,
+        romanceMovies: romanceMovies.results,
+        documentaries: documentaries.results,
+      },
+    }
+  } catch (error) {
+    console.log(error)
   }
 }
